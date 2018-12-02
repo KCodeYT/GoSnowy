56fcd5c5fe00c870c363cd3cf162bcd1bb8c4c61
Kevims <kcodeyt@gmail.com>
2018-12-02 20:57:11 +0100

    First commit

---
+++ src/main/java/de/kcodeyt/gosnowy/GoSnowy.java
@@ -0,0 +1,21 @@
+package de.kcodeyt.gosnowy;
+
+import io.gomint.plugin.Plugin;
+import io.gomint.plugin.PluginName;
+import io.gomint.plugin.Version;
+
+@PluginName("GoGroups")
+@Version(major = 1, minor = 3)
+public class GoSnowy extends Plugin {
+
+    @Override
+    public void onInstall() {
+
+    }
+
+    @Override
+    public void onUninstall() {
+
+    }
+
+}
